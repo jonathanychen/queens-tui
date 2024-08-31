fa600ec3e2de1ad11c051616d2a8472ead5d8f58
Jonathan Chen <jonathan.chenjc15243@gmail.com>
2024-08-31 16:54:43 -0400

    feat: convert tui to lipgloss rendering

--- view.go
+++ view.go
@@ -1,33 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/lipgloss/table"
+)
 
 func (m QueensModel) View() string {
 	s := "Queens\n\n"
 
 	s += fmt.Sprintf("%02d:%02d\n", m.timer/60, m.timer%60)
 
-	for i := 0; i < m.board.size+2; i++ {
-		s += "-"
-	}
-	s += fmt.Sprintln()
+	t := table.New().
+		Border(lipgloss.NormalBorder()).
+		BorderRow(true).
+		BorderColumn(true).
+		BorderStyle(lipgloss.NewStyle().Foreground(lipgloss.Color("99"))).
+		StyleFunc(m.LipglossStyleFunc()).
+		Rows(m.LipglossBoardContents()...)
 
-	for i := 0; i < m.board.size; i++ {
-		s += "|"
-		for j := 0; j < m.board.size; j++ {
-			if i == m.board.cursor.y && j == m.board.cursor.x {
-				s += "\033[36m*\033[0m"
-			} else {
-				s += fmt.Sprintf("%v", colorText(unicodeTile(m.board.grid[i][j]), m.board.colors[i][j]))
-			}
-		}
-		s += "|"
-		s += fmt.Sprintln()
-	}
-
-	for i := 0; i < m.board.size+2; i++ {
-		s += "-"
-	}
+	s += t.Render()
 
 	s += "\nPress q to quit.\n"
 
@@ -38,36 +31,57 @@ func (m QueensModel) View() string {
 	return s
 }
 
-func colorText(input string, color int) string {
-	var Reset = "\033[0m"
-	var Red = "\033[31m"
-	var Green = "\033[32m"
-	var Yellow = "\033[33m"
-	var Blue = "\033[34m"
-	var Magenta = "\033[35m"
-	// var Cyan = "\033[36m"
-	// var Gray = "\033[37m"
-	// var White = "\033[97m"
+func (m QueensModel) LipglossStyleFunc() func(row, col int) lipgloss.Style {
+	return func(row, col int) lipgloss.Style {
+		style := lipgloss.NewStyle().Padding(0, 1)
 
-	colorMap := map[int]string{}
-	colorMap[1] = Red
-	colorMap[2] = Green
-	colorMap[3] = Yellow
-	colorMap[4] = Blue
-	colorMap[5] = Magenta
+		if row >= 1 && row <= m.board.size && col >= 0 && col < m.board.size {
+			switch m.board.colors[row-1][col] {
+			case 1:
+				style = style.Background(lipgloss.Color("#FF0000"))
+			case 2:
+				style = style.Background(lipgloss.Color("#00FF00"))
+			case 3:
+				style = style.Background(lipgloss.Color("#FFFF00"))
+			case 4:
+				style = style.Background(lipgloss.Color("#0000FF"))
+			case 5:
+				style = style.Background(lipgloss.Color("#FF00FF"))
+			case 6:
+				style = style.Background(lipgloss.Color("#00FFFF"))
+			}
 
-	return colorMap[color] + input + Reset
+			// TODO: Figure out special styling for selected cursor box
+			if row-1 == m.board.cursor.y && col == m.board.cursor.x {
+				// style = style.Background(lipgloss.Color("#FFFFFF"))
+			}
+		}
+
+		return style
+	}
 }
 
-func unicodeTile(tile string) string {
-	res := ""
-	switch tile {
-	case " ":
-		res = "\u2588"
-	case "x":
-		res = "\u2A2F"
-	case "Q":
-		res = "\u2655"
+func (m QueensModel) LipglossBoardContents() [][]string {
+	res := [][]string{}
+
+	for i := 0; i < m.board.size; i++ {
+		row := []string{}
+		for j := 0; j < m.board.size; j++ {
+			if m.board.cursor.x == j && m.board.cursor.y == i && !m.gameOver {
+				row = append(row, "*")
+			} else {
+				switch m.board.grid[i][j] {
+				case " ":
+					row = append(row, " ")
+				case "x":
+					row = append(row, "\u2A2F")
+				case "Q":
+					row = append(row, "\u2655")
+				}
+			}
+		}
+		res = append(res, row)
 	}
+
 	return res
 }
